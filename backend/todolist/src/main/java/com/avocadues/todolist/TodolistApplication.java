b225be049359137573f01eaaaf728f16cefd49a9
scottsun17@gmail.com <scottsun17@gmail.com>
2020-10-04 20:09:21 -0400

    spring boot setup

--- backend/todolist/src/main/java/com/avocadues/todolist/TodolistApplication.java
+++ backend/todolist/src/main/java/com/avocadues/todolist/TodolistApplication.java
@@ -2,9 +2,19 @@ package com.avocadues.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class TodolistApplication {
+
+	@GetMapping(value="/hello")
+	public String getMethodName() {
+		return "咸鱼是变态";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
