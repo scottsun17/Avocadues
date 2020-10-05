06f691c1982308a51a98ab0d8bac4b2ed8eb64c7
younggg96 <yangguannggeng960123@gmail.com>
2020-10-04 20:31:04 -0400

    updated sign in details - FE

--- frontend/src/components/landingPage/signIn.js
+++ frontend/src/components/landingPage/signIn.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Link from '@material-ui/core/Link';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useForm } from "react-hook-form";
 import { Box, Button, Checkbox, Grid, Paper } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
+import img from '../../assets/imgs/logo.png';
+import '../../css/App.css'
 
 function Copyright() {
     return (
@@ -31,10 +31,10 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: theme.spacing(2),
         padding: theme.spacing(4)
     },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
+    // avatar: {
+    //     margin: theme.spacing(1),
+    //     backgroundColor: theme.palette.secondary.main,
+    // },
     form: {
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing(3),
@@ -53,9 +53,9 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         width: "100%",
         height: "3rem",
-        margin: theme.spacing(0.5, 0, 2, 0),
+        margin: theme.spacing(0.5, 0, 1.5, 0),
         padding: theme.spacing(1.5),
-    }
+    },
 }));
 
 export default function SignIn() {
@@ -72,34 +72,39 @@ export default function SignIn() {
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <Paper className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
+                {/* <Avatar className={classes.avatar}> */}
+                    {/* <LockOutlinedIcon /> */}
+                    <img width="80px" src={img} alt="logo"/>
+                {/* </Avatar> */}
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
+
                 <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
                     <Grid container alignItems="center">
                         <Grid item xs={12}>
-                            {/* <label htmlFor="email">
-                                <Typography variant="body1" component="span">Email</Typography>
-                            </label> */}
-                            <input name="email" type="email" ref={register} className={classes.inputCard} placeholder="Email" />
+                            <input name="email" type="email" ref={register({ required: true })} className={classes.inputCard} placeholder="Email *" />
+                            {errors.email && <Typography variant="caption" component="p" color="error" style={{ marginBottom: "4px" }}>This field is required</Typography>}
                         </Grid>
                         <Grid item xs={12}>
-                            {/* <label htmlFor="password">Password</label> */}
-                            <input name="password" type="password" ref={register} className={classes.inputCard} placeholder="Password" />
+                            <input name="password" type="password" ref={register({ required: true })} className={classes.inputCard} placeholder="Password *" />
+                            {errors.password && <Typography variant="caption" component="p" color="error" style={{marginBottom: "4px"}}>This field is required</Typography>}
                         </Grid>
                         <Grid item xs={12}>
-                            <Checkbox></Checkbox><Typography variant="body2" component="span">Remeber me</Typography>
+                            <Checkbox size="small"></Checkbox><Typography variant="body2" component="span" >Remeber me</Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            {/* <input type="submit" /> */}
-                            <Button color="primary" variant="contained" fullWidth type="submit" disableElevation className={classes.submit}>Sign In</Button>
+                            <Button variant="contained" fullWidth type="submit" disableElevation className={`btn-grad ${classes.submit}`}>Sign In</Button>
                         </Grid>
 
                     </Grid>
                 </form>
+                <Box mt={2}>
+                    <Typography component="span" variant="body2" className={classes.signup}>
+                        Don't have an account?
+                    <Link style={{marginLeft: "6px"}}>Sign Up</Link>
+                    </Typography>
+                </Box>
                 <Box mt={8} mb={2}>
                     <Copyright />
                 </Box>
