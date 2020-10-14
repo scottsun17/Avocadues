571f5f68894ae2e799579969a48abfbb4203ad69
scottsun17@gmail.com <scottsun17@gmail.com>
2020-10-14 18:35:41 -0400

    added functions to controller

--- backend/todolist/src/main/java/com/avocadues/todolist/entity/Category.java
+++ backend/todolist/src/main/java/com/avocadues/todolist/entity/Category.java
@@ -9,5 +9,38 @@ public class Category implements Serializable {
 	private String category_id;
 	private String category_name;
 	private String color;	
+	private String uid;
+
+	public String getCategoryId(){
+		return this.category_id;
+	}
+
+	public String getCategoryName(){
+		return this.category_name;
+	}
+
+	public String getColor(){
+		return this.color;
+	}
+
+	public String getUid(){
+		return this.uid;
+	}
+
+	public void setCategoryId(String category_id){
+		this.category_id = category_id;
+	}
+
+	public void setCategoryName(String category_name){
+		this.category_name = category_name;
+	}
+
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public void setUid(String uid){
+		this.uid = uid;
+	}
 }
 
--- backend/todolist/src/test/java/com/avocadues/todolist/mapper/DistrictMapperTest.java
+++ backend/todolist/src/test/java/com/avocadues/todolist/mapper/DistrictMapperTest.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.avocadues.todolist.TodolistApplicationTests;
 import com.avocadues.todolist.entity.District;
-import com.avocadues.todolist.entity.User;
+// import com.avocadues.todolist.entity.User;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,19 +31,5 @@ public class DistrictMapperTest extends TodolistApplicationTests{
     @Autowired
     private UserMapper userMapper;
 
-    @Test
-    void getAllUserAndCategory(){
-        List<User> list = userMapper.getAllUserAndCategory();
-        for(User user : list){
-            System.out.println(user);
-        }
-    }
 
-    @Test
-    void getCategoryList(){
-        List<User> list = userMapper.findCategoryListByUserId("1");
-        for(User user : list){
-            System.out.println(user.getCategoryId());
-        }
-    }
 }
